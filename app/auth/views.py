1b2abe1f5fe99fe8c8fdc9b7f7c876484944005f
mahmoudessam820 <esame4166@gmail.com>
2023-12-28 20:33:44 +0200

    added new columns to the user model and methods

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,14 @@
-import uuid
-from flask import (render_template, request, redirect, url_for, flash, session) 
+from flask import (
+    Response, 
+    render_template, 
+    request, 
+    redirect, 
+    url_for, 
+    flash
+)
+from wtforms import ValidationError 
+from sqlite3 import IntegrityError
+
 
 from . import auth 
 from ..forms.signin import SigninForm
@@ -10,73 +19,75 @@ from models.model import Users
 
 
 @auth.route('/signin', methods=["POST", "GET"])
-def signin():
+def signin() -> (Response | str):
+    if request.method == 'POST':
+        form = SigninForm()
+        if form.validate_on_submit():
+            existing_user = Users.query.filter_by(email=form.email.data).first()
+            if existing_user:
+                flash("Email already exists. Please choose a different email.", category='danger')
+                return redirect(url_for('auth.signin'))
+            
+            try:
+                Users.create_user(
+                    username=form.username.data,
+                    email=form.email.data,
+                    password1=form.password1.data
+                )
 
-    form = SigninForm()
+                new_user = Users.query.filter_by(email=form.email.data).first_or_404()
+                login_user(new_user)
 
-    if request.method == 'POST' and form.validate_on_submit():
-
-        password_hash = bcrypt.generate_password_hash(form.password1.data)
-
-        new_user = Users(
-            id=str(uuid.uuid4()),
-            username=form.username.data,
-            email=form.email.data,
-            password=password_hash
-        )
-
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user)
-
-            flash(f"You have successfully created a new account. Welcome {new_user.username}", 'success')
-
-            form.username.data = ''
-            form.email.data = ''
-            form.password1.data = ''
-
-            return redirect(url_for('market.market'))
-        except Exception as e:
-            db.session.rollback()
-            flash(f"An error occurred. Account could not be created. Error: {str(e)}", category='danger')
+                flash(f"You have successfully created a new account. Welcome {new_user.username}", 'success')
+                return redirect(url_for('market.market'))
+            except IntegrityError:
+                db.session.rollback()
+                flash("An error occurred. Account could not be created.", category='danger')
+            except ValidationError as e:
+                error_messages_str = " ".join(str(error) for error in e.messages)
+                flash(f"An error occurred: {error_messages_str}", category='danger')
+                return redirect(url_for('auth.signin'))
+        else:
+            for error_message in form.errors.values():
+                error_message_str = " ".join(str(message) for message in error_message)
+                flash(f"An error occurred: {error_message_str}", category='danger')
     else:
-        for error_message in form.errors.values():
-            flash(f"An error occurred: {error_message}. Account could not be created", category='danger')
+        form = SigninForm()
 
+    form.username.data = ''
+    form.email.data = ''
+    form.password1.data = ''
     return render_template('auth/signin.html', form=form)
 
 
 
 @auth.route('/login', methods=["POST", "GET"])
-def login():
-
+def login() -> (Response | str):
     form = LoginForm()
 
     if request.method == 'POST':
 
         if form.validate_on_submit():
-            user = Users.query.filter_by(email=form.email.data).first_or_404()
+            user = Users.query.filter_by(email=form.email.data).first()
 
-            if bcrypt.check_password_hash(user.password, form.password.data):
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                flash(f"You are now logged in as {user.username}", 'success')
-
-                form.email.data = ''
-                form.password.data = ''
+                flash('Login successful', 'success')
 
                 return redirect(url_for('market.market'))
             else:
-                flash('Login Unsuccessful. Please check email and password', 'danger')
+                flash('Invalid email or password', 'danger')
         else:
             flash('Form validation failed', 'danger')
 
+    form.email.data = ''
+    form.password.data = ''
     return render_template('auth/login.html', form=form)
 
 
 
 @auth.route('/logout', methods=["POST", "GET"])
-def logout():
+def logout() -> Response:
     logout_user()
     flash('You were successfully logout!', 'info')
     return redirect(url_for('main.home'))
--- models/model.py
+++ models/model.py
@@ -1,14 +1,14 @@
-
+import uuid
 from flask_login import UserMixin, current_user
 from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 
-from app import db, login_manager, admin
+from app import db, login_manager, admin, bcrypt 
 
-# ? Models
+# Models
 
-#! User Model
+# Users Model
 
 
 class Users(db.Model, UserMixin):
@@ -19,18 +19,55 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(length=50), nullable=False, unique=False)
     email = db.Column(db.String(length=150), nullable=False, unique=True)
     password = db.Column(db.String(length=255), nullable=False)
+    is_active = db.Column(db.Boolean, default=False, nullable=False)
+    is_staff = db.Column(db.Boolean, default=False, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
     def __init__(self, **data: dict) -> None:
         self.id = data.get('id')
         self.username = data.get('username')
         self.email = data.get('email')
         self.password = data.get('password')
+        self.is_active = data.get('is_active', True)
+        self.is_staff = data.get('is_staff', False)
+        self.is_admin = data.get('is_admin', False)
 
     def __repr__(self) -> str:
-        return f'<username: {self.username}, email: {self.email}, password: {self.password}>'
+        return f'<username: {self.username}, email: {self.email}>'
+
+    @classmethod
+    def create_admin(cls, **kwargs: dict) -> None:
+        password = bcrypt.generate_password_hash(kwargs.get('password1')).decode('utf8')
+        admin = cls(
+            id = str(uuid.uuid4()),
+            username = kwargs.get('username'),
+            email = kwargs.get('email'),
+            password = password,
+            is_admin = True,
+            is_staff = True,
+            is_active = True
+        )
+        db.session.add(admin)
+        db.session.commit()
+
+    @classmethod 
+    def create_user(cls, **kwargs: dict) -> None:
+        password = bcrypt.generate_password_hash(kwargs.get('password1')).decode('utf8')
+        user = cls(
+            id = str(uuid.uuid4()),
+            username = kwargs.get('username'),
+            email = kwargs.get('email'),
+            password = password,
+            is_active = True,
+            is_staff = False,
+            is_admin = False
+        )
+        db.session.add(user)
+        db.session.commit()
 
 
-#! Books Model
+
+# Books Model
 
 class Books(db.Model):
 
@@ -59,13 +96,18 @@ class Books(db.Model):
         return f"<Book Details: {self.title}, author: {self.author}, category: {self.category}, language: {self.language}, pages: {self.pages}, year: {self.year}, link: {self.link}>"
 
 
+
 # Login Manager
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(str(user_id))
 
 
-# Admin Views
+class Premissions:
+    pass
+
+
+# Register admin models view
 class UserView(ModelView):
     pass
 
@@ -77,6 +119,4 @@ class BookView(ModelView):
 admin.add_view(BookView(Books, db.session))
 
 
-
-
-
+# Secure admin view
