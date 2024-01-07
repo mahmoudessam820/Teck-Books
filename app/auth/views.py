10546cdadb40e014d27b45a6d1712bd26fadf6f7
mahmoudessam820 <esame4166@gmail.com>
2024-01-07 21:28:13 +0200

    modified the signin route to use create users methods

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,30 +23,46 @@ def signin() -> (Response | str):
     if request.method == 'POST':
         form = SigninForm()
         if form.validate_on_submit():
-            existing_user = Users.query.filter_by(email=form.email.data).first()
-            if existing_user:
-                flash("Email already exists. Please choose a different email.", category='danger')
-                return redirect(url_for('auth.signin'))
-            
-            try:
-                Users.create_user(
-                    username=form.username.data,
-                    email=form.email.data,
-                    password1=form.password1.data
-                )
+            existing_admin = Users.query.filter_by(is_admin=True).first()
 
-                new_user = Users.query.filter_by(email=form.email.data).first_or_404()
-                login_user(new_user)
+            if not existing_admin:
+                try:
+                    Users.create_admin(
+                        username=form.username.data,
+                        email=form.email.data,
+                        password1=form.password1.data
+                    )
+                    admin_user = Users.query.filter_by(is_admin=True).first()
+                    login_user(admin_user)
+                    flash("Admin user created successfully.", 'success')
+                    return redirect(url_for('market.market'))
+                except IntegrityError:
+                    db.session.rollback()
+                    flash("An error occurred. Admin account could not be created.", category='danger')
+                except ValidationError as e:
+                    error_messages_str = " ".join(str(error) for error in e.messages)
+                    flash(f"An error occurred: {error_messages_str}", category='danger')
+                    return redirect(url_for('auth.signin'))
+            else:
+                try:
+                    Users.create_user(
+                        username=form.username.data,
+                        email=form.email.data,
+                        password1=form.password1.data
+                    )
 
-                flash(f"You have successfully created a new account. Welcome {new_user.username}", 'success')
-                return redirect(url_for('market.market'))
-            except IntegrityError:
-                db.session.rollback()
-                flash("An error occurred. Account could not be created.", category='danger')
-            except ValidationError as e:
-                error_messages_str = " ".join(str(error) for error in e.messages)
-                flash(f"An error occurred: {error_messages_str}", category='danger')
-                return redirect(url_for('auth.signin'))
+                    new_user = Users.query.filter_by(email=form.email.data).first()
+                    login_user(new_user)
+
+                    flash(f"You have successfully created a new account. Welcome {new_user.username}", 'success')
+                    return redirect(url_for('market.market'))
+                except IntegrityError:
+                    db.session.rollback()
+                    flash("An error occurred. Account could not be created.", category='danger')
+                except ValidationError as e:
+                    error_messages_str = " ".join(str(error) for error in e.messages)
+                    flash(f"An error occurred: {error_messages_str}", category='danger')
+                    return redirect(url_for('auth.signin'))
         else:
             for error_message in form.errors.values():
                 error_message_str = " ".join(str(message) for message in error_message)
